a8c04e72219baae6efa6bee524ca7ac5d7bbcc7d
abhay kasyap <abhaykasyap2804@gmail.com>
2025-12-13 13:09:51 +0530

    fifth commit

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,11 +1,12 @@
-import ChatContainer from "../components/chatContainer";
+import ChatContainer from "../components/ChatContainer";
 import NoChatSelected from "../components/NoChatSelected";
 import Sidebar from "../components/Sidebar";
 import { chatStore } from "../store/chatStore";
+//import { chatStore } from "../store/chatStore";
 
 const HomePage = () => {
 
-    const { selectedUser } = chatStore();
+    const { selectedUser } = chatStore
 
     return <div className="flex flex-row min-h-screen overflow-hidden bg-base-100 rounded-lg shadow-cl w-full h-[calc(100vh-8rem)] ">
         <Sidebar />
