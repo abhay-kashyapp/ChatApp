7262b7f3cf2c21554914c150d71ddf66ddf4c05c
abhay kasyap <abhaykasyap2804@gmail.com>
2025-12-13 12:58:15 +0530

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Navigate, Route,Routes } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import HomePage from './Pages/HomePage.jsx';
-import SignUpPage from './Pages/SignupPage.jsx';
+import SignUpPage from './Pages/SignUpPage.jsx';
 import LoginPage from './Pages/LoginPage.jsx';
 import ProfilePage from './Pages/ProfilePage.jsx';
 import { Toaster } from 'react-hot-toast';
