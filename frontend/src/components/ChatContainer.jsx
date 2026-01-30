1bfcf4c52676d320b7c673d72463f633faa697ea
abhay kasyap <abhaykasyap2804@gmail.com>
2026-01-30 15:16:32 +0530

    Fix: case-sensitivity in ChatHeader import

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,19 +1,18 @@
-import ChatHeader from "./chatHeader";
+import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 
 
 
-const ChatContainer = () => { 
+const ChatContainer = () => {
   return <div className="flex flex-col h-full bg-base-100 shadow-lg md:w-auto ">
     <ChatHeader />
     <div className="flex-1 overflow-y-auto ">
-      <Messages />   
+      <Messages />
     </div>
     <div className="flex-shrink-1 p-4 bg-base-200">
-    <MessageInput />
+      <MessageInput />
     </div>
   </div>
 }
 export default ChatContainer;
-  
